89a94faf2092493f420ed4228827bd295749c48e
Julien Dehos <dehos@univ-littoral.fr>
2025-07-02 16:30:23 +0200

    clean code

--- src/Main.hs
+++ src/Main.hs
@@ -3,21 +3,17 @@
 
 module Main where
 
--- import Control.Monad
+import Control.Monad (void)
 import Data.Function ((&))
 import Data.Foldable (traverse_)
 import Language.Javascript.JSaddle 
--- import Data.Map as Map ((!?), adjust, keys, notMember)
 import Miso
--- import Miso.Lens as Lens
 import Miso.Canvas as Canvas
-import Miso.String (MisoString, ms)
 
 import THREE.BoxGeometry
 import THREE.Internal
 import THREE.Light
 import THREE.Mesh
--- import THREE.MeshBasicMaterial
 import THREE.MeshLambertMaterial
 import THREE.Object3D
 import THREE.PerspectiveCamera
@@ -76,7 +72,7 @@ draw = do
   light1 <- THREE.PointLight.new
   light1 & intensity .= 300
   light1 ^. position !.. setXYZ 8 8 8
-  scene1 & add light1
+  void $ scene1 & add light1
 
   material1 <- THREE.MeshLambertMaterial.new
   geometry1 <- THREE.SphereGeometry.new
@@ -87,12 +83,10 @@ draw = do
   material2 <- THREE.MeshLambertMaterial.new
   material2 & THREE.MeshLambertMaterial.map .= Just texture2
   geometry2 <- THREE.BoxGeometry.new (1, 1, 1, Just 1, Just 1, Just 1)
-  -- geometry2 <- THREE.BoxGeometry.new (1, 1, 1, Nothing, Nothing, Nothing)
   mesh2 <- THREE.Mesh.new (geometry2, material2)
   (mesh2 ^. position) !.. setXYZ 1 0 0 
 
   traverse_ (`add` scene1) [mesh1, mesh2]
-  -- scene1 & add mesh1 >>= add mesh2
 
   camera1 <- THREE.PerspectiveCamera.new (70, winWidth / winHeight, 0.1, 100)
   camera1 & position !. z .= 6
@@ -107,28 +101,6 @@ draw = do
     )
 
   domElement renderer1 >>= appendInBody 
-
-
-{-
-draw :: Three ()
-draw = do
-  width <- windowInnerWidth
-  height <- windowInnerHeight
-  let value = realToFrac (width `div` height)
-  scene <- THREE.Scene.new
-  camera <- THREE.PerspectiveCamera.new (75.0, value, 0.1, 1000)
-  renderer <- THREE.WebGLRenderer.new
-  renderer & THREE.WebGLRenderer.setSize (width, height, True)
-  geometry <- THREE.BoxGeometry.new (10,10,10,Nothing,Nothing,Nothing)
-  material <- THREE.MeshBasicMaterial.new Nothing
-  material & THREE.MeshBasicMaterial.color .= "#000fff"
-  cube <- THREE.Mesh.new (geometry,material)
-  void (scene & THREE.Object3D.add cube)
-  camera & THREE.Object3D.position !. z .= 300
-  renderer & THREE.WebGLRenderer.render (scene, camera)
-  cube & THREE.Object3D.rotation !. x += 0.1
-  cube & THREE.Object3D.rotation !. y += 0.1
--}
 
 ----------------------------------------------------------------------
 -- update handler
